c6994c8ffe165ff5deaded516b2545a33ec73daa
kushalkumarghosh <ghoshkushalcse@gmail.com>
2025-09-26 20:02:38 +0600

    updated

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,11 +12,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        \App\Models\User::factory()->create([
+            'name' => 'Admin',
+            'email' => 'ghoshkushalcse@gmail.com',
+            'password' => bcrypt('passw0rrmsd'),
+            'usertype' => 1,
+        ]);
     }
 }
